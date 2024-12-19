d6fd72a2389c6dd42dc49224c0d977fe6b5b937e
bitcoinflywheel <info@bitcoinflywheel.org>
2024-12-18 19:54:01 -0600

    deployment actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'bitcoinflywheel', // Usually your GitHub org/user name.
   projectName: 'bitcoinflywheel', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
